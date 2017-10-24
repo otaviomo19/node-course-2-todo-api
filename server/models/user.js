7b2d0ef911918c925c297ddcefa563936d066b9b
Otavio <otaviomota19@hotmail.com>
2017-10-23 21:46:07 -0300

    Update config to use json file

--- server/models/user.js
+++ server/models/user.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
-const bcrypt = require('bcryptjs')
+const bcrypt = require('bcryptjs');
 
 var UserSchema = new mongoose.Schema({
   email: {
@@ -43,7 +43,7 @@ UserSchema.methods.toJSON = function () {
 UserSchema.methods.generateAuthToken = function () {
   var user = this;
   var access = 'auth';
-  var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+  var token = jwt.sign({_id: user._id.toHexString(), access}, process.env.JWT_SECRET).toString();
 
   user.tokens.push({access, token});
 
@@ -52,22 +52,22 @@ UserSchema.methods.generateAuthToken = function () {
   });
 };
 
-UserSchema.methods.removeToken = function (token){
+UserSchema.methods.removeToken = function (token) {
   var user = this;
 
   return user.update({
-    $pull:{
-      tokens:{token}
+    $pull: {
+      tokens: {token}
     }
-  })
-}
+  });
+};
 
 UserSchema.statics.findByToken = function (token) {
   var User = this;
   var decoded;
 
   try {
-    decoded = jwt.verify(token, 'abc123');
+    decoded = jwt.verify(token, process.env.JWT_SECRET);
   } catch (e) {
     return Promise.reject();
   }
@@ -100,16 +100,16 @@ UserSchema.statics.findByCredentials = function (email, password) {
   });
 };
 
-UserSchema.pre('save',function(next){
+UserSchema.pre('save', function (next) {
   var user = this;
 
-  if(user.isModified('password')){
-      bcrypt.genSalt(10, (err,salt) => {
-        bcrypt.hash(user.password,salt, (err,hash) => {
-          user.password = hash;
-          next();
-        })
-      })
+  if (user.isModified('password')) {
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        user.password = hash;
+        next();
+      });
+    });
   } else {
     next();
   }
